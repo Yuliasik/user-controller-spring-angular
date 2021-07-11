c38da8fa3194729fe52e6b063455224878378aa0
yulia_04 <yulichka.dudij@gmail.com>
2021-07-12 02:47:43 +0300

    Сreated a project with users and function adding a new user

---
+++ src/main/java/com/practice/demo/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.practice.demo.repository;
+
+import com.practice.demo.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long>{}
